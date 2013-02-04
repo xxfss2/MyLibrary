8a93c2e7ed5665086c64f3f032e1d548bc784586
xxf <xxfss2@163.com>
2013-02-04 15:06:32 +0800

    Add xxf namespace assembly

---
+++ Xxf/Extension/DataConventerExtension.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Xxf.Extension
+{
+    public static class DataConventerExtension
+    {
+        public static decimal? TryParseDecimal(this string val)
+        {
+            if (string.IsNullOrEmpty(val))
+                return null;
+            decimal result;
+            if (decimal.TryParse(val, out result))
+                return result;
+            else
+                return null;
+        }
+
+        public static int? TryParseInt(this string val)
+        {
+            if (string.IsNullOrEmpty(val))
+                return null;
+            int result;
+            if (int.TryParse(val, out result))
+                return result;
+            else
+                return null;
+        }
+
+        public static bool? TryParseBoolean(this string val)
+        {
+            if (string.IsNullOrEmpty(val))
+                return null;
+            bool result;
+            if (bool.TryParse(val, out result))
+                return result;
+            else return null;
+        }
+
+        public static DateTime? TryParseDatetime(this string val)
+        {
+            if (string.IsNullOrEmpty(val))
+                return null;
+            DateTime result;
+            if (DateTime.TryParse(val, out result))
+                return result;
+            else return null;
+        }
+    }
+}
