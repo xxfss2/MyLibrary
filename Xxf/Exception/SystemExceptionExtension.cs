29ca20feecda4a1f0c96a6615005f4f1b3ffa3ab
xxf <xxfss2@163.com>
2013-12-13 17:26:06 +0800

    添加Exception处理模块
    SystemExceptionExtension类根据异常示例获取Message和内部异常的Message

---
+++ Xxf/Exception/SystemExceptionExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Xxf
+{
+    public static class SystemExceptionExtension
+    {
+        /// <summary>
+        /// 获取异常及内部异常（一层）的Message，并反序化Exception示例保存至程序ExcepitonLog目录(未完成)
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <returns></returns>
+        public static string Messages(this SystemException ex)
+        {
+            string msg = ex.Message;
+            if (ex.InnerException != null)
+                msg += Environment.NewLine + ex.InnerException.Message;
+            return msg;
+        }
+    }
+}
